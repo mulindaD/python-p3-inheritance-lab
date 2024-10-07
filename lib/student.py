6b7d02c70eb5ef61b5d59c9d8e010df2870556af
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-10-07 18:46:39 +0300

    Inheritance lab completed

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,18 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name, knowledge=[]):
+        super().__init__(first_name, last_name)
+        self._knowledge = knowledge
+
+    @property
+    def knowledge(self):
+        return self._knowledge
     
-    def learn(self):
-        pass
+    @knowledge.setter
+    def knowledge(self, str):
+        self._knowledge.append(str)
+
+    def learn(self, str):
+        return self._knowledge.append(str)
